39ed39111f5beddff9b7b9e9d511a919248fdd7a
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-04-02 12:00:38 +0200

    feat(extension) `Instance` accepts Wasm bytes in its constructor.

--- src/instance.rs
+++ src/instance.rs
@@ -1,19 +1,10 @@
 //! The `Instance` Python object to build WebAssembly instances.
 
 use crate::value::{get_wasm_value, wasm_value_into_python_object, Value};
-use cpython::{PyObject, PyResult};
+use cpython::{PyBytes, PyObject, PyResult};
 use generational_arena::{Arena, Index};
 use std::cell::RefCell;
 use wasmer_runtime::{self as runtime, imports, instantiate, Value as WasmValue};
-
-static WASM: &'static [u8] = &[
-    // The module above compiled to bytecode goes here.
-    0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x01, 0x06, 0x01, 0x60, 0x01, 0x7f, 0x01, 0x7f,
-    0x03, 0x02, 0x01, 0x00, 0x07, 0x0b, 0x01, 0x07, 0x61, 0x64, 0x64, 0x5f, 0x6f, 0x6e, 0x65, 0x00,
-    0x00, 0x0a, 0x09, 0x01, 0x07, 0x00, 0x20, 0x00, 0x41, 0x01, 0x6a, 0x0b, 0x00, 0x1a, 0x04, 0x6e,
-    0x61, 0x6d, 0x65, 0x01, 0x0a, 0x01, 0x00, 0x07, 0x61, 0x64, 0x64, 0x5f, 0x6f, 0x6e, 0x65, 0x02,
-    0x07, 0x01, 0x00, 0x01, 0x00, 0x02, 0x70, 0x30,
-];
 
 thread_local! {
     pub static WASM_INSTANCES: RefCell<Arena<runtime::Instance>> = RefCell::new(Arena::new());
@@ -34,9 +25,10 @@ impl Drop for InnerInstance {
 py_class!(pub class Instance |py| {
     data instance: InnerInstance;
 
-    def __new__(_cls) -> PyResult<Instance> {
+    def __new__(_cls, bytes: PyBytes) -> PyResult<Instance> {
+        let bytes = bytes.data(py);
         let imports = imports! {};
-        let instance = instantiate(WASM, &imports).unwrap();
+        let instance = instantiate(bytes, &imports).unwrap();
         let index = WASM_INSTANCES.with(|f| f.borrow_mut().insert(instance));
 
         Instance::create_instance(
@@ -53,6 +45,7 @@ py_class!(pub class Instance |py| {
                 .into_iter()
                 .map(|value_object| get_wasm_value(py, &value_object))
                 .collect();
+
         let index = self.instance(py).index;
         let results = WASM_INSTANCES.with(
             |instances| {
