659eaf9080b618d2daad9214e6b62f851a1fc253
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-04-10 15:25:17 -0700

    feat(extension) Add the `Shell` structure.

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,53 @@
 extern crate cpython;
 
 use cpython::PyBytes;
+use std::{ops::Deref, thread};
 
 mod instance;
 mod value;
 
 use instance::{validate, Instance};
 use value::Value;
+
+/// A `Shell` is a thread-safe wrapper over a value that will fail if
+/// used in another thread. Why? All data used by Python must be
+/// thread, but some WebAssembly data cannot be thread-safe. The
+/// program will compile and Python will be able to use it, but Python
+/// must not pass the data between thread. The documentation will
+/// specify it.
+pub struct Shell<T> {
+    /// The thread ID where the datum has been created.
+    thread_id: thread::ThreadId,
+
+    /// The datum.
+    value: T,
+}
+
+impl<T> Shell<T> {
+    pub fn new(value: T) -> Self {
+        Self {
+            thread_id: thread::current().id(),
+            value,
+        }
+    }
+}
+
+/// A `Shell` is sendable.
+unsafe impl<T> Send for Shell<T> {}
+
+/// Dereferences the value if it's inside the same thread than the
+/// creation thread.
+impl<T> Deref for Shell<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        if thread::current().id() != self.thread_id {
+            panic!("The current `Shell` cannot be dereferenced in a different thread.");
+        }
+
+        &self.value
+    }
+}
 
 // Declare the module.
 py_module_initializer!(libwasm, initlibwasm, PyInit_wasm, |python, module| {
