a29c44b3cd048a2cd21b7d83717529056e91bb46
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-04-10 15:43:05 -0700

    feat(extension) Replace the thread local arena by `Shell`.

--- src/instance.rs
+++ src/instance.rs
@@ -4,39 +4,12 @@ use crate::{
     error::new_runtime_error,
     memory_view::{new_memory_view, MemoryView},
     value::{get_wasm_value, wasm_value_into_python_object, Value},
+    Shell,
 };
 use cpython::{PyBytes, PyObject, PyResult, Python};
-use generational_arena::{Arena, Index};
-use std::cell::RefCell;
 use wasmer_runtime::{
     self as runtime, imports, instantiate, validate as wasm_validate, Export, Value as WasmValue,
 };
-
-/// `wasmer_runtime::Instance` isn't thread-safe, and it's somewhat
-/// complex to make it entirely thread-safe. Instead, this trick is
-/// used: Create a thread local storage key, that contains an arena,
-/// that itself contains `wasmer_runtime::Instance`s. Each entry is
-/// indexed by a `generational_arena::Index` value. This index is held
-/// by the Python `Instance` object.
-thread_local! {
-    pub static WASM_INSTANCES: RefCell<Arena<runtime::Instance>> = RefCell::new(Arena::new());
-}
-
-/// Holds all the Wasm instance information.
-pub struct InnerInstance {
-    /// Index of the Wasm instance in `WASM_INSTANCES`.
-    index: Index,
-}
-
-/// When the Python object is dropped, this structure is dropped, and
-/// then, the Wasm instance is removed from `WASM_INSTANCES`.
-impl Drop for InnerInstance {
-    fn drop(&mut self) {
-        WASM_INSTANCES.with(|instances| {
-            instances.borrow_mut().remove(self.index);
-        });
-    }
-}
 
 /// The Python `Instance` class.
 ///
@@ -52,20 +25,14 @@ impl Drop for InnerInstance {
 /// result = instance.call('add_one', [Value::from_i32(1)])
 /// ```
 py_class!(pub class Instance |py| {
-    data instance: InnerInstance;
+    data instance: Shell<runtime::Instance>;
 
     def __new__(_cls, bytes: PyBytes) -> PyResult<Instance> {
         let bytes = bytes.data(py);
         let imports = imports! {};
         let instance = instantiate(bytes, &imports).unwrap();
-        let index = WASM_INSTANCES.with(|f| f.borrow_mut().insert(instance));
 
-        Instance::create_instance(
-            py,
-            InnerInstance {
-                index
-            }
-        )
+        Instance::create_instance(py, Shell::new(instance))
     }
 
     def call(&self, function_name: &str, function_arguments: Vec<Value> = Vec::new()) -> PyResult<PyObject> {
@@ -75,38 +42,25 @@ py_class!(pub class Instance |py| {
                 .map(|value_object| get_wasm_value(py, &value_object))
                 .collect();
 
-        let index = self.instance(py).index;
-        let results = WASM_INSTANCES.with(
-            |instances| {
-                let instances = instances.borrow();
-                let instance = instances.get(index).unwrap();
-
-                instance.dyn_func(function_name).unwrap().call(function_arguments.as_slice()).unwrap()
-            }
-        );
+        let instance = self.instance(py);
+        let results = instance.dyn_func(function_name).unwrap().call(function_arguments.as_slice()).unwrap();
 
         Ok(wasm_value_into_python_object(py, &results[0]))
     }
 
     def memory_view(&self, offset: usize = 0) -> PyResult<MemoryView> {
-        let index = self.instance(py).index;
-        let memory = WASM_INSTANCES.with(
-            |instances| {
-                let instances = instances.borrow();
-                let instance = instances.get(index).unwrap();
-
-                instance
-                    .exports()
-                    .find_map(
-                        |(_, export)| {
-                            match export {
-                                Export::Memory(memory) => Some(memory),
-                                _ => None
-                            }
+        let memory =
+            self
+                .instance(py)
+                .exports()
+                .find_map(
+                    |(_, export)| {
+                        match export {
+                            Export::Memory(memory) => Some(memory),
+                            _ => None
                         }
-                    )
-            }
-        );
+                    }
+                );
 
         match memory {
             Some(memory) => {
