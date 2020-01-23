04f3133b2588f54908e6e7c8e5ea17b574f92165
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2020-01-23 17:53:52 +0100

    test(module) Test the `Module.exports` getter.

--- tests/test_module.py
+++ tests/test_module.py
@@ -29,6 +29,62 @@ def test_failed_to_compile():
 def test_instantiate():
     assert Module(TEST_BYTES).instantiate().exports.sum(1, 2) == 3
 
+def test_exports():
+    assert Module(TEST_BYTES).exports == [
+        {
+            "name": "memory",
+            "kind": "memory",
+        },
+        {
+            "name": "__heap_base",
+            "kind": "global",
+        },
+        {
+            "name": "__data_end",
+            "kind": "global",
+        },
+        {
+            "name": "sum",
+            "kind": "function",
+        },
+        {
+            "name": "arity_0",
+            "kind": "function",
+        },
+        {
+            "name": "i32_i32",
+            "kind": "function",
+        },
+        {
+            "name": "i64_i64",
+            "kind": "function",
+        },
+        {
+            "name": "f32_f32",
+            "kind": "function",
+        },
+        {
+            "name": "f64_f64",
+            "kind": "function",
+        },
+        {
+            "name": "i32_i64_f32_f64_f64",
+            "kind": "function",
+        },
+        {
+            "name": "bool_casted_to_i32",
+            "kind": "function",
+        },
+        {
+            "name": "string",
+            "kind": "function",
+        },
+        {
+            "name": "void",
+            "kind": "function",
+        },
+    ]
+
 def test_serialize():
     assert type(Module(TEST_BYTES).serialize()) == bytes
 
