d21ce8f92cf2751f4879c98539a4968edf37c984
Ivan Enderlin <ivan@mnt.io>
2020-04-16 14:03:22 +0200

    doc(examples) Use native `int` type.

--- examples/imported_function.py
+++ examples/imported_function.py
@@ -5,7 +5,7 @@ __dir__ = os.path.dirname(os.path.realpath(__file__))
 
 wasm_bytes = open(__dir__ + '/imported_function.wasm', 'rb').read()
 
-def sum(x: 'i32', y: 'i32') -> 'i32':
+def sum(x: int, y: int) -> int:
     return x + y
 
 instance = Instance(
