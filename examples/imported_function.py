2047c770031f260f1f8aed213125548e450ad0c8
Ivan Enderlin <ivan@mnt.io>
2020-04-28 17:30:38 +0200

    doc(examples) Use `ImportObject` in `imported_function`.

--- examples/imported_function.py
+++ examples/imported_function.py
@@ -1,4 +1,4 @@
-from wasmer import Instance
+from wasmer import Instance, Module, ImportObject
 import os
 
 __dir__ = os.path.dirname(os.path.realpath(__file__))
@@ -18,3 +18,14 @@ instance = Instance(
 )
 
 print(instance.exports.sum_plus_one(1, 2))
+
+module = Module(wasm_bytes)
+import_object = module.generate_import_object()
+import_object.extend({
+    "env": {
+        "sum": sum
+    }
+})
+instance = module.instantiate(import_object)
+
+print(instance.exports.sum_plus_one(3, 4))
