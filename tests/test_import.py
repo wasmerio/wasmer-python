576cdb0e8b75999691c11efebf336c6e3cd0e246
Ivan Enderlin <ivan@mnt.io>
2020-06-02 10:40:37 +0200

    test(import) Skip `test_import_object_extend` if host functions aren't supported.

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,4 +1,4 @@
-from wasmer import Module, ImportKind
+from wasmer import Module, ImportKind, Features
 import inspect
 import os
 import pytest
@@ -11,6 +11,7 @@ def test_import_object_empty():
 
     assert len(import_object.import_descriptors()) == 0
 
+@pytest.mark.skipif(Features.host_functions() == False, reason='Host functions are not supported for this platform')
 def test_import_object_extend():
     import_object = Module(TEST_BYTES).generate_import_object()
     import_object.extend({
