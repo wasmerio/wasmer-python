537d285f3fe3bc05b858186eb902b570cc79f31e
Ivan Enderlin <ivan@mnt.io>
2020-06-02 11:28:07 +0200

    doc(features) Document the `Features` class.

--- src/features.rs
+++ src/features.rs
@@ -1,10 +1,14 @@
 use pyo3::prelude::*;
 
+/// `Features` is a Python class used to test whether a specific
+/// feature is enabled or not.
 #[pyclass]
 pub struct Features {}
 
 #[pymethods]
 impl Features {
+    /// Check whether host functions are enabled.
+    #[text_signature = "()"]
     #[staticmethod]
     pub fn host_functions() -> bool {
         if cfg!(all(unix, target_arch = "x86_64")) {
