6a033a745dfa91401ea894a86861a28e7dc104e8
Ivan Enderlin <ivan@mnt.io>
2020-06-02 10:10:04 +0200

    doc(example) Update the `wasi` example.

--- examples/wasi.py
+++ examples/wasi.py
@@ -1,4 +1,4 @@
-from wasmer import Module, ImportObject, WasiVersion, WasiStateBuilder
+from wasmer import Module, ImportObject, WasiVersion, Wasi
 import os
 
 __dir__ = os.path.dirname(os.path.realpath(__file__))
@@ -6,17 +6,14 @@ __dir__ = os.path.dirname(os.path.realpath(__file__))
 wasm_bytes = open(__dir__ + "/wasi.wasm", "rb").read()
 module = Module(wasm_bytes)
 
-wasi_state_builder = \
-    WasiStateBuilder("wasi_test_program"). \
+wasi = \
+    Wasi("wasi_test_program"). \
         argument("--test"). \
         environment("COLOR", "true"). \
         environment("APP_SHOULD_LOG", "false"). \
         map_directory("the_host_current_dir", ".")
 
-import_object = module.generate_wasi_import_object(
-    wasi_state_builder,
-    module.wasi_version(False)
-)
+import_object = wasi.generate_import_object_for_module(module)
 
 instance = module.instantiate(import_object)
 instance.exports._start()
