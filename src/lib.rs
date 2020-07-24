48dfce8f0fa8c5420aeeb2229b9f01ccd58b8b41
Ivan Enderlin <ivan@mnt.io>
2020-07-24 13:44:59 +0200

    feat: Implement the `Module.exports` getter.
    It returns a list of named tuple `ExportType`.

--- src/lib.rs
+++ src/lib.rs
@@ -13,17 +13,25 @@ mod r#extern;
 mod module;
 mod store;
 
-/// This extension allows to manipulate and to execute WebAssembly binaries.
+/// This extension allows to compile and to execute WebAssembly.
 #[pymodule]
 fn wasmer(py: Python, module: &PyModule) -> PyResult<()> {
     let enum_module = py.import("enum")?;
+    let collections_module = py.import("collections")?;
 
+    // Constants.
     module.add("__version__", env!("CARGO_PKG_VERSION"))?;
     module.add("__core_version__", env!("WASMER_VERSION"))?;
+
+    // Functions.
     module.add_wrapped(wrap_pyfunction!(wat2wasm))?;
     module.add_wrapped(wrap_pyfunction!(wasm2wat))?;
+
+    // Classes.
     module.add_class::<module::Module>()?;
     module.add_class::<store::Store>()?;
+
+    // Enums.
     module.add(
         "ExternType",
         enum_module.call1(
@@ -40,6 +48,12 @@ fn wasmer(py: Python, module: &PyModule) -> PyResult<()> {
                 ],
             ),
         )?,
+    )?;
+
+    // NamedTuples.
+    module.add(
+        "ExportType",
+        module::create_export_type_class(py, collections_module)?,
     )?;
 
     Ok(())
--- src/module.rs
+++ src/module.rs
@@ -1,10 +1,12 @@
+use crate::r#extern::ExternType;
 use crate::store::Store;
 use crate::wasmer_inner::wasmer;
 use pyo3::{
     exceptions::RuntimeError,
     prelude::*,
-    types::{PyAny, PyBytes, PyList, PyString},
+    types::{PyAny, PyBytes, PyList, PyString, PyTuple},
 };
+use std::{cell::RefCell, thread_local};
 
 #[pyclass(unsendable)]
 #[text_signature = "(store, bytes)"]
@@ -64,14 +66,53 @@ impl Module {
         Ok(())
     }
 
+    #[getter]
+    fn exports<'p>(&self, py: Python<'p>) -> PyResult<&'p PyList> {
+        Ok(PyList::new(
+            py,
+            self.inner
+                .exports()
+                .map(|export_type| {
+                    new_export_type(py, export_type.name(), ExternType::from(export_type.ty()))
+                })
+                .collect::<PyResult<Vec<_>>>()?,
+        ))
+    }
+
     fn custom_sections<'p>(&self, py: Python<'p>, name: &str) -> &'p PyList {
         PyList::new(
             py,
             self.inner
                 .custom_sections(name)
                 .map(|custom_section| PyBytes::new(py, &*custom_section))
-                .collect::<Vec<_>>()
-                .into_iter(),
+                .collect::<Vec<_>>(),
         )
     }
+}
+
+thread_local! {
+    static EXPORT_TYPE_CLASS: RefCell<Option<PyObject>> = RefCell::new(None);
+}
+
+pub(crate) fn create_export_type_class<'p>(
+    py: Python<'p>,
+    collections_module: &'p PyModule,
+) -> PyResult<&'p PyAny> {
+    let class =
+        collections_module.call1("namedtuple", PyTuple::new(py, &["ExportType", "name type"]))?;
+
+    EXPORT_TYPE_CLASS.with(|export_type_class| {
+        *export_type_class.borrow_mut() = Some(class.to_object(py));
+    });
+
+    Ok(class)
+}
+
+fn new_export_type<'p, 'a>(py: Python<'p>, name: &'a str, ty: ExternType) -> PyResult<PyObject> {
+    EXPORT_TYPE_CLASS.with(|export_type_class| {
+        export_type_class.borrow().as_ref().unwrap().call1(
+            py,
+            PyTuple::new(py, &[name.to_object(py), ty.to_object(py)]),
+        )
+    })
 }
