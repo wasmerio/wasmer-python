acf03b63ca556291af31373cb30372def5548580
Ivan Enderlin <ivan@mnt.io>
2020-08-20 17:23:45 +0200

    test: Update the `Type` test suite.

--- tests/test_type.py
+++ tests/test_type.py
@@ -3,9 +3,11 @@ from enum import IntEnum
 
 def test_type():
     assert issubclass(Type, IntEnum)
-    assert len(Type) == 5
+    assert len(Type) == 7
     assert Type.I32 == 1
     assert Type.I64 == 2
     assert Type.F32 == 3
     assert Type.F64 == 4
     assert Type.V128 == 5
+    assert Type.EXTERN_REF == 6
+    assert Type.FUNC_REF == 7
