47557fa9d3bc1c726c1de7452d4aecbd55821016
Ivan Enderlin <ivan@mnt.io>
2020-08-20 17:37:59 +0200

    test: Split the test suite into `_instance` and `_function`.

---
+++ tests/test_function.py
@@ -0,0 +1,48 @@
+import wasmer
+from wasmer import Instance, Module, Store, Function
+import os
+import pytest
+
+here = os.path.dirname(os.path.realpath(__file__))
+TEST_BYTES = open(here + '/tests.wasm', 'rb').read()
+
+def instance():
+    Instance(Module(Store(), TEST_BYTES))
+
+def value_with_type(value):
+    return (value, type(value))
+
+def test_export_function():
+    assert isinstance(instance().exports.sum, Function)
+
+def test_basic_sum():
+    assert value_with_type(instance().exports.sum(1, 2)) == (3, int)
+
+def test_call_arity_0():
+    assert value_with_type(instance().exports.arity_0()) == (42, int)
+
+def test_call_i32_i32():
+    assert value_with_type(instance().exports.i32_i32(7)) == (7, int)
+
+def test_call_i64_i64():
+    assert value_with_type(instance().exports.i64_i64(7)) == (7, int)
+
+def test_call_f32_f32():
+    assert value_with_type(instance().exports.f32_f32(7.)) == (7., float)
+
+def test_call_f64_f64():
+    assert value_with_type(instance().exports.f64_f64(7.)) == (7., float)
+
+def test_call_i32_i64_f32_f64_f64():
+    assert round(instance().exports.i32_i64_f32_f64_f64(1, 2, 3.4, 5.6), 6) == (
+        1 + 2 + 3.4 + 5.6
+    )
+
+def test_call_bool_casted_to_i32():
+    assert value_with_type(instance().exports.bool_casted_to_i32()) == (1, int)
+
+def test_call_string():
+    assert instance().exports.string() == 1048576
+
+def test_call_void():
+    assert instance().exports.void() == None
--- tests/test_instance.py
+++ tests/test_instance.py
@@ -1,16 +1,11 @@
 import wasmer
-from wasmer import Instance, Module, Store, Exports, Function
-import inspect
+from wasmer import Instance, Module, Store, Exports
 import os
 import pytest
-import sys
 
 here = os.path.dirname(os.path.realpath(__file__))
 TEST_BYTES = open(here + '/tests.wasm', 'rb').read()
 INVALID_TEST_BYTES = open(here + '/invalid.wasm', 'rb').read()
-
-def value_with_type(value):
-    return (value, type(value))
 
 def test_version():
     assert isinstance(wasmer.__version__, str)
@@ -38,43 +33,6 @@ def test_export_does_not_exist():
     exception = context_manager.value
     assert str(exception) == 'Export `foo` does not exist.'
 
-def test_export_function():
-    instance = Instance(Module(Store(), TEST_BYTES))
-
-    assert isinstance(instance.exports.sum, Function)
-    assert value_with_type(instance.exports.sum(1, 2)) == (3, int)
-
-def test_basic_sum():
-    assert value_with_type(Instance(Module(Store(), TEST_BYTES)).exports.sum(1, 2)) == (3, int)
-
-def test_call_arity_0():
-    assert value_with_type(Instance(Module(Store(), TEST_BYTES)).exports.arity_0()) == (42, int)
-
-def test_call_i32_i32():
-    assert value_with_type(Instance(Module(Store(), TEST_BYTES)).exports.i32_i32(7)) == (7, int)
-
-def test_call_i64_i64():
-    assert value_with_type(Instance(Module(Store(), TEST_BYTES)).exports.i64_i64(7)) == (7, int)
-
-def test_call_f32_f32():
-    assert value_with_type(Instance(Module(Store(), TEST_BYTES)).exports.f32_f32(7.)) == (7., float)
-
-def test_call_f64_f64():
-    assert value_with_type(Instance(Module(Store(), TEST_BYTES)).exports.f64_f64(7.)) == (7., float)
-
-def test_call_i32_i64_f32_f64_f64():
-    assert round(Instance(Module(Store(), TEST_BYTES)).exports.i32_i64_f32_f64_f64(1, 2, 3.4, 5.6), 6) == (
-        1 + 2 + 3.4 + 5.6
-    )
-
-def test_call_bool_casted_to_i32():
-    assert value_with_type(Instance(Module(Store(), TEST_BYTES)).exports.bool_casted_to_i32()) == (1, int)
-
-def test_call_string():
-    assert Instance(Module(Store(), TEST_BYTES)).exports.string() == 1048576
-
-def test_call_void():
-    assert Instance(Module(Store(), TEST_BYTES)).exports.void() == None
 #
 #def test_memory_view():
 #    assert isinstance(Instance(TEST_BYTES).memory.uint8_view(), Uint8Array)
