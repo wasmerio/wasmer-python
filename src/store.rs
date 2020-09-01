b636abc5e585e852e2d73eb0d5146dde44c6f092
Ivan Enderlin <ivan@mnt.io>
2020-09-01 11:24:38 +0200

    doc: Write documentation of `Store`.

--- src/store.rs
+++ src/store.rs
@@ -1,7 +1,27 @@
 use crate::wasmer_inner::wasmer;
 use pyo3::prelude::*;
 
+/// The store represents all global state that can be manipulated by
+/// WebAssembly programs. It consists of the runtime representation of
+/// all instances of functions, tables, memories, and globals that
+/// have been allocated during the lifetime of the abstract machine.
+///
+/// The `Store` holds the engine (that is —amongst many things— used
+/// to compile the WebAssembly bytes into a valid module artifact), in
+/// addition to the `Tunables` (that are used to create the memories,
+/// tables and globals).
+///
+/// Specification: https://webassembly.github.io/spec/core/exec/runtime.html#store
+///
+/// ## Example
+///
+/// ```py
+/// from wasmer import Store
+///
+/// store = Store()
+/// ```
 #[pyclass]
+#[text_signature = "(/)"]
 pub struct Store {
     inner: wasmer::Store,
 }
