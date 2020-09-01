3d3c64f4325b84e05d7580a53cbebae667bd3b46
Ivan Enderlin <ivan@mnt.io>
2020-09-01 16:12:11 +0200

    doc: Write documentation of `ImportObject`.

--- src/import_object.rs
+++ src/import_object.rs
@@ -8,7 +8,128 @@ use pyo3::{
     types::{PyDict, PyString},
 };
 
+/// An `ImportObject` represents all of the import data used when
+/// instantiating a WebAssembly module.
+///
+/// # Example
+///
+/// Importing a function, `math.sum`, and call it through the exported
+/// `add_one` function:
+///
+/// ```py
+/// from wasmer import Store, Module, Instance, ImportObject, Function
+/// def sum(x: int, y: int) -> int:
+///     return x + y
+///
+/// store = Store()
+/// module = Module(
+///     store,
+///     """
+///     (module
+///       (import "math" "sum" (func $sum (param i32 i32) (result i32)))
+///       (func (export "add_one") (param i32) (result i32)
+///         local.get 0
+///         i32.const 1
+///         call $sum))
+///     """
+/// )
+///
+/// import_object = ImportObject()
+/// import_object.register(
+///     "math",
+///     {
+///         "sum": Function(store, sum)
+///     }
+/// )
+///
+/// instance = Instance(module, import_object)
+///
+/// assert instance.exports.add_one(1) == 2
+/// ```
+///
+/// Importing a memory:
+///
+/// ```py
+/// from wasmer import Store, Module, Instance, Memory, MemoryType, ImportObject
+///
+/// store = Store()
+/// module = Module(
+///     store,
+///     """
+///     (module
+///       (import "env" "memory" (memory $memory 1))
+///       (func (export "increment")
+///         i32.const 0
+///         i32.const 0
+///         i32.load    ;; load 0
+///         i32.const 1
+///         i32.add     ;; add 1
+///         i32.store   ;; store at 0
+///         ))
+///     """
+/// )
+///
+/// memory = Memory(store, MemoryType(1, shared=False))
+/// view = memory.uint8_view(offset=0)
+///
+/// import_object = ImportObject()
+/// import_object.register(
+///     "env",
+///     {
+///         "memory": memory
+///     }
+/// )
+///
+/// instance = Instance(module, import_object)
+///
+/// assert view[0] == 0
+/// instance.exports.increment()
+/// assert view[0] == 1
+/// instance.exports.increment()
+/// assert view[0] == 2
+/// ```
+///
+/// Importing a global:
+///
+/// ```py
+/// from wasmer import Store, Module, Instance, ImportObject, Global, Value
+///
+/// store = Store()
+/// module = Module(
+///     store,
+///     """
+///     (module
+///       (import "env" "global" (global $global (mut i32)))
+///       (func (export "read_g") (result i32)
+///         global.get $global)
+///       (func (export "write_g") (param i32)
+///         local.get 0
+///         global.set $global))
+///     """
+/// )
+///
+/// global_ = Global(store, Value.i32(7), mutable=True)
+///
+/// import_object = ImportObject()
+/// import_object.register(
+///     "env",
+///     {
+///         "global": global_
+///     }
+/// )
+///
+/// instance = Instance(module, import_object)
+///
+/// assert instance.exports.read_g() == 7
+/// global_.value = 153
+/// assert instance.exports.read_g() == 153
+/// instance.exports.write_g(11)
+/// assert global_.value == 11
+/// ```
+///
+/// etc.
 #[pyclass(unsendable)]
+#[text_signature = "()"]
 pub struct ImportObject {
     inner: wasmer::ImportObject,
 }
@@ -30,11 +151,44 @@ impl ImportObject {
         ImportObject::raw_new(Default::default())
     }
 
+    /// Checks whether the import object contains a specific namespace.
+    ///
+    /// ## Example
+    ///
+    /// ```py
+    /// from wasmer import ImportObject
+    ///
+    /// import_object = ImportObject()
+    ///
+    /// assert import_object.contains_namespace("foo") == False
+    /// ```
     #[text_signature = "($self, namespace_name)"]
     fn contains_namespace(&self, namespace_name: &str) -> bool {
         self.inner.contains_namespace(namespace_name)
     }
 
+    /// Registers a set of `Function`, `Memory`, `Global` or `Table`
+    /// to a particular namespace.
+    ///
+    /// ## Example
+    ///
+    /// ```py
+    /// from wasmer import ImportObject, Function, Memory, MemoryType
+    ///
+    /// store = Store()
+    ///
+    /// def sum(x: int, y: int) -> int:
+    ///     return x + y
+    ///
+    /// import_object = ImportObject()
+    /// import_object.register(
+    ///     "env",
+    ///     {
+    ///         "sum": Function(store, sum),
+    ///         "memory": Memory(store, MemoryType(1, shared=False))
+    ///     }
+    /// )
+    /// ```
     #[text_signature = "($self, namespace_name, namespace)"]
     fn register(&mut self, namespace_name: &str, namespace: &PyDict) -> PyResult<()> {
         let mut wasmer_namespace = wasmer::Exports::new();
