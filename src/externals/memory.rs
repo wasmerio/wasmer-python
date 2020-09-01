1bbbcb1dd59803d7f9a2f670fe9641b69c0b7e4d
Ivan Enderlin <ivan@mnt.io>
2020-09-01 15:46:49 +0200

    doc: Write documentation of `Memory`.

--- src/externals/memory.rs
+++ src/externals/memory.rs
@@ -7,7 +7,50 @@ use crate::{
 };
 use pyo3::{exceptions::RuntimeError, prelude::*};
 
+/// A WebAssembly memory instance.
+///
+/// A memory instance is the runtime representation of a linear
+/// memory. It consists of a vector of bytes and an optional maximum
+/// size.
+///
+/// The length of the vector always is a multiple of the WebAssembly
+/// page size, which is defined to be the constant 65536 – abbreviated
+/// 64Ki. Like in a memory type, the maximum size in a memory
+/// instance is given in units of this page size.
+///
+/// A memory created by the host or in WebAssembly code will be accessible and
+/// mutable from both host and WebAssembly.
+///
+/// Specification: https://webassembly.github.io/spec/core/exec/runtime.html#memory-instances
+///
+/// ## Example
+///
+/// Creates a `Memory` from scratch:
+///
+/// ```py
+/// from wasmer import Store, Memory, MemoryType
+///
+/// store = Store()
+/// memory_type = MemoryType(3, shared=False)
+/// memory = Memory(store, memory_type)
+///
+/// assert memory.size == 3
+/// ```
+///
+/// Gets a memory from the exports of an instance:
+///
+/// ```py
+/// from wasmer import Store, Module, Instance, Memory
+///
+/// module = Module(Store(), open('tests/tests.wasm', 'rb').read())
+/// instance = Instance(module)
+///
+/// memory = instance.exports.memory
+///
+/// assert isinstance(memory, Memory)
+/// ```
 #[pyclass(unsendable)]
+#[text_signature = "(store, memory_type)"]
 pub struct Memory {
     inner: wasmer::Memory,
 }
@@ -32,16 +75,59 @@ impl Memory {
         ))
     }
 
+    /// Returns the size (in pages) of the `Memory`.
+    ///
+    /// ## Example
+    ///
+    /// ```py
+    /// from wasmer import Store, Module, Instance
+    ///
+    /// module = Module(Store(), open('tests/tests.wasm', 'rb').read())
+    /// instance = Instance(module)
+    /// memory = instance.exports.memory
+    ///
+    /// assert memory.size == 17
+    /// ```
     #[getter]
     fn size(&self) -> u32 {
         self.inner.size().0
     }
 
+    /// Returns the size (in bytes) of the `Memory`.
+    ///
+    /// ## Example
+    ///
+    /// ```py
+    /// from wasmer import Store, Module, Instance
+    ///
+    /// module = Module(Store(), open('tests/tests.wasm', 'rb').read())
+    /// instance = Instance(module)
+    /// memory = instance.exports.memory
+    ///
+    /// assert memory.size == 1114112
+    /// ```
     #[getter]
     fn data_size(&self) -> u64 {
         self.inner.data_size()
     }
 
+    /// Grow memory by the specified amount of WebAssembly pages.
+    ///
+    /// ## Example
+    ///
+    /// ```py
+    /// from wasmer import Store, Memory, MemoryType
+    ///
+    /// store = Store()
+    /// memory_type = MemoryType(3, shared=False)
+    /// memory = Memory(store, memory_type)
+    ///
+    /// assert memory.size == 3
+    ///
+    /// memory.grow(2)
+    ///
+    /// assert memory.size == 5
+    /// ```
     #[text_signature = "($self, number_of_pages)"]
     fn grow(&self, number_of_pages: u32) -> PyResult<u32> {
         self.inner
@@ -50,12 +136,41 @@ impl Memory {
             .map_err(to_py_err::<RuntimeError, _>)
     }
 
+    /// Creates a Python buffer to read and write the memory data. See
+    /// the `Buffer` class to learn more.
+    ///
+    /// ## Example
+    ///
+    /// ```py
+    /// from wasmer import Store, Memory, MemoryType, Buffer
+    ///
+    /// store = Store()
+    /// memory_type = MemoryType(3, shared=False)
+    /// memory = Memory(store, memory_type)
+    ///
+    /// assert isinstance(memory.buffer, Buffer)
+    /// ```
     #[getter]
     fn buffer(&self) -> Buffer {
         Buffer::new(self.inner.clone())
     }
 
-    #[text_signature = "($self, offset=0)"]
+    /// Creates a read-and-write view over the memory data where
+    /// elements are of kind `uint8`. See the `Uint8Array` view to
+    /// learn more.
+    ///
+    /// ## Examples
+    ///
+    /// ```py
+    /// from wasmer import Store, Memory, MemoryType, Uint8Array
+    ///
+    /// store = Store()
+    /// memory_type = MemoryType(3, shared=False)
+    /// memory = Memory(store, memory_type)
+    ///
+    /// assert isinstance(memory.uint8_view(offset=42), Uint8Array)
+    /// ```
+    #[text_signature = "($self, /, offset=0)"]
     #[args(offset = 0)]
     fn uint8_view(&self, offset: usize) -> Uint8Array {
         Uint8Array {
@@ -64,7 +179,10 @@ impl Memory {
         }
     }
 
-    #[text_signature = "($self, offset=0)"]
+    /// Creates a read-and-write over the memory data where elements
+    /// are of kind `int8`. See the `Int8Array` view to learn more,
+    /// and the `Memory.uint8_view` method to see an example.
+    #[text_signature = "($self, /, offset=0)"]
     #[args(offset = 0)]
     fn int8_view(&self, offset: usize) -> Int8Array {
         Int8Array {
@@ -73,7 +191,10 @@ impl Memory {
         }
     }
 
-    #[text_signature = "($self, offset=0)"]
+    /// Creates a read-and-write over the memory data where elements
+    /// are of kind `uint16`. See the `Uint16Array` view to learn
+    /// more, and the `Memory.uint8_view` method to see an example.
+    #[text_signature = "($self, /, offset=0)"]
     #[args(offset = 0)]
     fn uint16_view(&self, offset: usize) -> Uint16Array {
         Uint16Array {
@@ -82,7 +203,10 @@ impl Memory {
         }
     }
 
-    #[text_signature = "($self, offset=0)"]
+    /// Creates a read-and-write over the memory data where elements
+    /// are of kind `int16`. See the `Int16Array` view to learn more,
+    /// and the `Memory.uint8_view` method to see an example.
+    #[text_signature = "($self, /, offset=0)"]
     #[args(offset = 0)]
     fn int16_view(&self, offset: usize) -> Int16Array {
         Int16Array {
@@ -91,7 +215,10 @@ impl Memory {
         }
     }
 
-    #[text_signature = "($self, offset=0)"]
+    /// Creates a read-and-write over the memory data where elements
+    /// are of kind `uint32`. See the `Uint32Array` view to learn
+    /// more, and the `Memory.uint8_view` method to see an example.
+    #[text_signature = "($self, /, offset=0)"]
     #[args(offset = 0)]
     fn uint32_view(&self, offset: usize) -> Uint32Array {
         Uint32Array {
@@ -100,7 +227,10 @@ impl Memory {
         }
     }
 
-    #[text_signature = "($self, offset=0)"]
+    /// Creates a read-and-write over the memory data where elements
+    /// are of kind `int32`. See the `Int32Array` view to learn more,
+    /// and the `Memory.uint8_view` method to see an example.
+    #[text_signature = "($self, /, offset=0)"]
     #[args(offset = 0)]
     fn int32_view(&self, offset: usize) -> Int32Array {
         Int32Array {
@@ -109,6 +239,22 @@ impl Memory {
         }
     }
 
+    /// Gets the memory type, of kind `MemoryType`.
+    ///
+    /// ## Example
+    ///
+    /// ```py
+    /// from wasmer import Store, Memory, Instance
+    ///
+    /// module = Module(Store(), open('tests/tests.wasm', 'rb').read())
+    /// instance = Instance(module)
+    /// memory = instance.exports.memory
+    /// memory_type = memory.type
+    ///
+    /// assert memory_type.minimum == 17
+    /// assert memory_type.maximum == None
+    /// assert memory_type.shared == False
+    /// ```
     #[getter(type)]
     fn ty(&self) -> MemoryType {
         self.inner.ty().into()
