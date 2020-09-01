f2f28360363cc7afb4a5e88c9f8ebbb3b571e2cc
Ivan Enderlin <ivan@mnt.io>
2020-09-01 14:50:25 +0200

    doc: Write documentation of `Instance`.

--- src/instance.rs
+++ src/instance.rs
@@ -4,11 +4,95 @@ use crate::{
 };
 use pyo3::{exceptions::RuntimeError, prelude::*};
 
+/// A WebAssembly instance is a stateful, executable instance of a
+/// WebAssembly `Module`.
+///
+/// Instance objects contain all the exported WebAssembly functions,
+/// memories, tables and globals that allow interacting with
+/// WebAssembly.
+///
+/// Specification: https://webassembly.github.io/spec/core/exec/runtime.html#module-instances
+///
+/// ## Example
+///
+/// Example without an import object. The following creates a module
+/// with a `sum` exported function that sum two integers.
+///
+/// ```py
+/// from wasmer import Store, Module, Instance
+///
+/// module = Module(
+///     Store(),
+///     """
+///     (module
+///       (type (func (param i32 i32) (result i32)))
+///       (func (type 0)
+///         local.get 0
+///         local.get 1
+///         i32.add)
+///       (export "sum" (func 0)))
+///     """
+/// )
+/// instance = Instance(module)
+///
+/// assert instance.exports.sum(1, 2) == 3
+/// ```
+///
+/// Example with an import object. The following creates a module that
+/// (i) imports a `sum` function from the `math` namespace, and (ii)
+/// exports a `add_one` function that adds 1 to any given integer (by
+/// using the `math.sum` function).
+///
+/// ```py
+/// from wasmer import Store, Module, Instance, ImportObject, Function
+///
+/// # Let's define the `sum` function!
+/// def sum(x: int, y: int) -> int:
+///     return x + y
+///
+/// # Let's build a store, as usual.
+/// store = Store()
+///
+/// # Let's compile the WebAssembly module.
+/// module = Module(
+///     store,
+///     """
+///     (module
+///       (import "math" "sum" (func $sum (param i32 i32) (result i32)))
+///       (func (export "add_one") (param i32) (result i32)
+///         local.get 0
+///         i32.const 1
+///         call $sum))
+///     """
+/// )
+///
+/// # Now, let's create an import object, and register the `sum`
+/// # function.
+/// import_object = ImportObject()
+/// import_object.register(
+///     "math",
+///     {
+///         "sum": Function(store, sum)
+///     }
+/// )
+///
+/// # Here we go, let's instantiate the module with the import object!
+/// instance = Instance(module, import_object)
+///
+/// # Let's test it!
+/// assert instance.exports.add_one(41) == 42
+/// ```
 #[pyclass(unsendable)]
 #[text_signature = "(module, import_object)"]
 pub struct Instance {
     #[allow(unused)]
     inner: wasmer::Instance,
+
+    /// The exports of the instance, as an object of kind `Exports`.
+    ///
+    /// ## Example
+    ///
+    /// See the `Exports` class.
     #[pyo3(get)]
     exports: Py<Exports>,
 }
