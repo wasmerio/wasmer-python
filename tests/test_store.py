4f126e7197ccb94eedc4857217a3b47fe3ecf387
Ivan Enderlin <ivan@mnt.io>
2020-09-18 10:04:42 +0200

    test: Skip `test_store` on Windows.

--- tests/test_store.py
+++ tests/test_store.py
@@ -16,6 +16,7 @@ def test_store_defaults():
     assert store.engine_name == 'jit'
     assert store.compiler_name == 'cranelift'
 
+@pytest.mark.skipif(platform.system() == 'Windows', reason='Wasmer (`master`) has some troubles with JIT on Windows for the moment.')
 def test_store_with_various_engines_and_compilers():
     engines = [
         engine.JIT,
